8282ae2b7e5501ec3a85e17552a1dd7bb843e43c
Edward <wardvisual@gmail.com>
2022-06-08 12:45:34 +0800

    [CHORE] clean code structure

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
-
-#include "src/app.h"
-
 using namespace std;
 
-int main() {
-
-  render_application();
-
-  for (int i = 0; i <= 3; i++) {
-  }
-
-  return 0;
+int main () {
+   return 0;
 }
