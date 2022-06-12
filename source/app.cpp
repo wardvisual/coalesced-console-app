d9314b8d88f5a5f3c59a14d412cbfc16587350f3
Edward <wardvisual@gmail.com>
2022-06-12 18:41:23 +0800

    [CHORE] setup a function comment template

--- source/app.cpp
+++ source/app.cpp
@@ -1,3 +1,26 @@
 #include "app.h"
+#include <iostream>
 
 using namespace std;
+
+/**
+ * @name app
+ * @brief description about the structure
+ * @property {is_readu, render}
+ */
+struct {
+  bool is_ready() { return true; }
+  void render() { cout << "Render"; }
+} app;
+
+/**
+ * @brief description about the function
+ * @param parameter_name
+ * @param parameter_name
+ * @returns return data type
+ */
+void render_application() {
+  if (app.is_ready()) {
+    app.render();
+  }
+}
