dac09580ecfc7047e935f38bbe52e33a744ec970
Edward <wardvisual@gmail.com>
2022-06-13 17:23:46 +0800

    [CHORE] mini changes

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
+/**
+ * @file main.cpp
+ * @brief This is the entry point of the application
+ * @date 2022-06-13
+ *
+ */
+
 #include <iostream>
 
-int main(int, char **) { std::cout << "Hello, world!\n"; }
+/* Custom header files */
+#include "includes/change_color.h"
+
+using namespace std;
+
+/**
+ * @brief This is the main function of the execution function.
+ * @return int
+ */
+
+int main() {
+  cout << "Texst";
+
+  change_color(RED);
+  cout << "Texs21";
+
+  return 0;
+}
