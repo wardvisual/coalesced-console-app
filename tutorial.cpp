e0e3bcb22422ccf0f4690d5ee2054fff99080155
Edward <wardvisual@gmail.com>
2022-06-25 19:41:08 +0800

    [CHORE] add test file

--- tutorial.cpp
+++ tutorial.cpp
@@ -5,8 +5,24 @@ using namespace std;
 int main() {
   char user_input;
   char want_to_continue;
-
-  cout << "----------------------------------\n";
+  
+  
+  cout << "n 1 ";
+  cin >> user_input;
+  
+  cout << "\nn 2 ";
+  cin >> want_to_continue;
+   
+  if(user_input && want_to_continue) {
+    cout << "hey";    
+  } else {
+    cout << "zp";    
+  }
 
   return 0;
 }
+
+
+
+// https://github.com/catchorg/Catch2/blob/devel/examples/010-TestCase.cpp
+// https://github.com/eshiofune/100-days-of-code/blob/master/cpp/BlinkingLights/main.cpp
