81465b96c211bd123b90c3a765e3c104ec6112db
Edward <wardvisual@gmail.com>
2022-06-25 19:41:41 +0800

    [CHORE] clean main file

--- main.cpp
+++ main.cpp
@@ -1,42 +1,5 @@
+#include <iostream>
 
-/**
- * It takes a function pointer as an argument and calls it
- * 
- * @param render_app This is the function that will be called inside the show_border function.
- */
- /**
-  * @file main.cpp
-  * @brief This is the entry point of the application.
-  */
+using namespace std;
 
- #include <iostream>
-
- /* Custom header files */
- #include "includes/apply_color.h"
-
- using namespace std;
-
- void show_border(void (*render_app)()) {
-   apply_color(WHITE);
-   cout << "\n-------------------------------------------------\n";
-   render_app();
-   apply_color(WHITE);
-   cout << "\n-------------------------------------------------\n";
- }
-
- void render_app() {
-   apply_color(BLUE);
-   cout << "Lazada: Order niyo Sir nahulog sa kanal HAHAHAHA";
-   apply_color(GREEN);
-   cout << "\nCustomer: Luh?";
- }
-
- int main() {
-
-   show_border(&render_app);
-
-   return 0;
- }
-
-// https://github.com/catchorg/Catch2/blob/devel/examples/010-TestCase.cpp
-// https://github.com/eshiofune/100-days-of-code/blob/master/cpp/BlinkingLights/main.cpp
+int main() { return 0; }
