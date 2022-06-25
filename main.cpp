53d42e7a977045f7e3ecbb333660855d65dacc41
Edward <wardvisual@gmail.com>
2022-06-25 22:07:52 +0800

    [CHORE] update main file

--- main.cpp
+++ main.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+string generateSymbol(string symbol, int count) {
+  string generatedSymbol;
+
+  for (int i = 1; i <= count; i++) {
+    generatedSymbol += symbol;
+  }
+
+  return generatedSymbol;
+}
+
+void showWelcomeMessage() { cout << "hey"; }
+
 int main() {
 
-  renderApplication();
+  cout << "\n\n" << generateSymbol("_", 50) << "\n\n";
+
+  showWelcomeMessage();
+
+  cout << "\n\n" << generateSymbol("_", 50) << "\n\n";
 
   return 0;
 }
