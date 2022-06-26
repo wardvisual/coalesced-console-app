0bb8e591150b5cc325939c1df4184a05c114ab61
Edward <wardvisual@gmail.com>
2022-06-26 09:17:10 +0800

    [CHORE] mini changes on main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <windows.h>
 
+#include "./helpers/border/border.hpp"
+
 using namespace std;
 
 string generateSymbol(string symbol, int count) {
@@ -15,15 +17,6 @@ string generateSymbol(string symbol, int count) {
 
 void showWelcomeMessage() { cout << "hey"; }
 
-void centerContent(short x, short y) {
-  COORD coordinates;
-
-  coordinates.X = x;
-  coordinates.Y = y;
-
-  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinates);
-}
-
 int main() {
 
   // cout << "\n\n" << generateSymbol("_", 50) << "\n";
@@ -34,32 +27,7 @@ int main() {
   // cout << "\n\n" << generateSymbol("_", 50) << "\n";
   // cout << "\n" << generateSymbol("_", 50) << "\n\n";
 
-  const int width = 88;
-  const int height = 32;
-
-  cout << "\n";
-  for (int i = 0; i < width - 30; i++) {
-    cout << "||";
-  }
-
-  cout << endl;
-
-  for (int i = 0; i < height; i++) {
-    for (int j = 0; j < width; j++) {
-      if (j == 0) {
-        cout << "\t\t\t||";
-      }
-      cout << " ";
-      if (j == width - 1)
-        cout << "||";
-    }
-    cout << endl;
-  }
-
-  for (int i = 0; i < width - 30; i++) {
-    cout << "||";
-  }
-
+  generateBorder(88, 32);
   // cout << " ";
   return 0;
 }
