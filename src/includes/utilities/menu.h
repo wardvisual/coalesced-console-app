1c05be8335d9a7f95dd918064164b7b038bec00f
Edward <wardvisual@gmail.com>
2022-06-27 07:32:15 +0800

    [CHORE] clean menu structure

--- src/includes/utilities/menu.h
+++ src/includes/utilities/menu.h
@@ -1,8 +1,8 @@
-#include "./constants/escapeSequence.h"
-#include "./constants/keycode.h"
-#include "./constants/runtime.h"
-#include "./generateId.h"
-#include "./screen.h"
+#include "constants/escapeSequence.h"
+#include "constants/keycode.h"
+#include "constants/runtime.h"
+#include "generateId.h"
+#include "screen.h"
 
 #include "windows.h"
 #include <conio.h>
@@ -21,72 +21,112 @@ struct MenuStruct {
   int alignmentY;
   int colorRange;
   int fontSize;
-  string content;
+  string name;
   int maxItem;
 
-  // MenuStruct *createFunction() {}
+  MenuStruct items[maxItem];
+  Item itemList[maxItem];
 
-  // MenuStruct *createVariable() {}
+  void createMenu(MenuStruct menuStruct) {
+    char keyCode;
+    int counter = 2;
 
-  MenuStruct *addItem(Item _item) {
+    struct Item itemList[menuStruct.maxItem];
+
+    for (int i = 0;;) {
+      modifyScreenContentPlacement(menuStruct);
+
+      /* Read a single character from the console without echoing the character
+       */
+      keyCode = _getch();
+
+      if (keyCode == ESCAPE_KEY && (counter >= 2 && counter <= 3))
+        counter--;
+
+      if (keyCode == DOWN_ARROW_KEY && (counter >= 1 && counter <= 2))
+        counter++;
+
+      if (keyCode == ESCAPE_KEY)
+        exit(EXIT_FAILURE);
+
+      if (keyCode == CURRIAGE_RETURN) {
+        for (int i = 1; i < menuStruct.maxItem; i++) {
+          if (counter == itemList[i].id) {
+            itemList[i].action();
+          }
+        }
+      }
+
+      for (int i = 1; i < menuStruct.maxItem; i++) {
+        itemList[i].colorRange = 7; // default color white
+
+        if (counter == i) {
+          itemList[i].colorRange = 12; // color red
+        }
+      }
+    }
+
+    for (int i = 0; i < this->maxItem; i++) {
+      this->items[i] = menuStruct;
+    }
+  }
+
+  void addItem(Item _item) {
     Item item;
 
     item.id = generateId();
     item.text = _item.text;
     item.action = _item.action;
-
-    return this;
   }
 
-  MenuStruct *setAlignment(short x, short y) {
+  void setName(string name) {
+  }
+
+  void setAlignment(short x, short y) {
     this->alignmentX = x;
     this->alignmentY = y;
-
-    return this;
   }
 
-  MenuStruct *setMaxItem(int maxItem) {
+  void setMaxItem(int maxItem) {
     this->maxItem = maxItem;
-
-    return this;
   }
 };
 
-void createMenu(MenuStruct menuStruct) {
-  char keyCode;
-  int counter = 2;
+// void createMenu(MenuStruct menuStruct) {
+//   char keyCode;
+//   int counter = 2;
 
-  struct Item itemList[menuStruct.maxItem];
+//   struct Item itemList[menuStruct.maxItem];
 
-  for (int i = 0;;) {
-    modifyScreenContentPlacement(menuStruct);
+//   for (int i = 0;;) {
+//     modifyScreenContentPlacement(menuStruct);
 
-    /* Read a single character from the console without echoing the character */
-    keyCode = _getch();
+//     /* Read a single character from the console without echoing the character
+//     */ keyCode = _getch();
 
-    if (keyCode == ESCAPE_KEY && (counter >= 2 && counter <= 3))
-      counter--;
+//     if (keyCode == ESCAPE_KEY && (counter >= 2 && counter <= 3))
+//       counter--;
 
-    if (keyCode == DOWN_ARROW_KEY && (counter >= 1 && counter <= 2))
-      counter++;
+//     if (keyCode == DOWN_ARROW_KEY && (counter >= 1 && counter <= 2))
+//       counter++;
 
-    if (keyCode == ESCAPE_KEY)
-      exit(EXIT_FAILURE);
+//     if (keyCode == ESCAPE_KEY)
+//       exit(EXIT_FAILURE);
 
-    if (keyCode == CURRIAGE_RETURN) {
-      for (int i = 1; i < menuStruct.maxItem; i++) {
-        if (counter == itemList[i].id) {
-          itemList[i].action();
-        }
-      }
-    }
+//     if (keyCode == CURRIAGE_RETURN) {
+//       for (int i = 1; i < menuStruct.maxItem; i++) {
+//         if (counter == itemList[i].id) {
+//           itemList[i].action();
+//         }
+//       }
+//     }
 
-    for (int i = 1; i < menuStruct.maxItem; i++) {
-      itemList[i].colorRange = 7; // default color white
+//     for (int i = 1; i < menuStruct.maxItem; i++) {
+//       itemList[i].colorRange = 7; // default color white
 
-      if (counter == i) {
-        itemList[i].colorRange = 12; // color red
-      }
-    }
-  }
-}
+//       if (counter == i) {
+//         itemList[i].colorRange = 12; // color red
+//       }
+//     }
+//   }
+// }
