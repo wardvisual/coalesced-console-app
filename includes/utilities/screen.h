2b45af216efdffe3ea8aaea5c470f18fdd25382d
Edward <wardvisual@gmail.com>
2022-06-26 11:26:18 +0800

    [CHORE] clean and rename screen.h

--- includes/utilities/screen.h
+++ includes/utilities/screen.h
@@ -1,16 +1,20 @@
 #include "windows.h"
 
+// Defining a struct called ScreenInfo.
 struct ScreenInfo {
-  int gotoxyX;
-  int gotoxyY;
+  int alignX;
+  int alignY;
   int colorRange;
   int fontSize;
   string content;
 };
 
 void modifyScreenContentPlacement(ScreenInfo screenInfo) {
-  centerContent(screenInfo.gotoxyX, screenInfo.gotoxyY);
+  // Modifying the screen content placement.
+  centerContent(screenInfo.alignX, screenInfo.alignY);
   applyColor(screenInfo.colorRange);
   adjustFont(screenInfo.fontSize);
+
+  // Printing the content of the screenInfo.content variable.
   cout << screenInfo.content;
 }
