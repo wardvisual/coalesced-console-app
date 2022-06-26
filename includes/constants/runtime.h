fd26eff89a6010dd66e0f1e19bb3217b7d3fcf40
Edward <wardvisual@gmail.com>
2022-06-26 15:11:57 +0800

    [CHORE] add exit success constant

--- includes/constants/runtime.h
+++ includes/constants/runtime.h
@@ -2,3 +2,7 @@
 /* Reports the termination when some error or interruption occurs during the
  execution of the program. */
 #define EXIT_FAILURE 1
+/*  Exit Success is indicated by exit(0) statement which means successful
+ termination of the program, i.e. program has been executed without any error
+ or interrupt. */
+#define EXIT_SUCCESS 0
