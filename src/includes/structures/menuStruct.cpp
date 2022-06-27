e51ee4411fe90396e9dcca1c380efddfa3ce2d8a
Edward <wardvisual@gmail.com>
2022-06-27 20:44:27 +0800

    [CHORE] create separete file for menuStruct

---
+++ src/includes/structures/menuStruct.cpp
@@ -0,0 +1,13 @@
+#ifndef MENU_STRUCT
+#define MENU_STRUCT
+
+struct MenuStruct {
+  int maxItem;
+  int menuItemAlignmentX;
+  int menuItemAlignmentY;
+  int colorRange;
+  int fontSize;
+  string name;
+};
+
+#endif 
