afce97aa55554b4a3fcaed009283e7c471a866d9
Edward <wardvisual@gmail.com>
2022-06-27 20:43:53 +0800

    [CHORE] simplify code structure

--- src/includes/utilities/menu.cpp
+++ src/includes/utilities/menu.cpp
@@ -1,93 +1,121 @@
-#include "./constants/escapeSequence.h"
-#include "./constants/keycode.h"
-#include "./constants/runtime.h"
-#include "generateId.h"
-#include "screen.h"
-
 #include "windows.h"
 #include <conio.h>
 #include <vector>
+
+#include "../constants/escapeSequence.cpp"
+#include "../constants/keycode.cpp"
+#include "../constants/runtime.cpp"
+
+#include "../structures/screenStruct.cpp"
+#include "../structures/menuStruct.cpp"
+
+#include "generateId.cpp"
+#include "screen.cpp"
+
+void displaySecondSemMenu() {
+  ScreenInfo screenStruct;
+
+  screenStruct.alignmentX = 30;
+  screenStruct.alignmentY = 5;
+  screenStruct.colorRange = 2;
+  screenStruct.name = "Second Sem";
+  modifyScreenContentPlacement(screenStruct);
+}
+
+void displayMidtermMenu() {
+  ScreenInfo screenStruct;
+
+  screenStruct.alignmentX = 30;
+  screenStruct.alignmentY = 5;
+  screenStruct.colorRange = 2;
+  screenStruct.name = "Midterm";
+
+  modifyScreenContentPlacement(screenStruct);
+}
+
+void displayFinalSemMenu() {
+  ScreenInfo screenStruct;
+
+  screenStruct.alignmentX = 30;
+  screenStruct.alignmentY = 5;
+  screenStruct.colorRange = 2;
+  screenStruct.name = "Final Sem";
+
+  modifyScreenContentPlacement(screenStruct);
+}
 
 struct Item {
   int id;
   string text;
   int colorRange;
-  void action();
+  void setAction(void *action()) { action(); };
 };
 
 // Defining a struct called ScreenStruct.
-struct MenuStruct {
-  int alignmentX;
-  int alignmentY;
-  int colorRange;
-  int fontSize;
-  string name;
-  int maxItem;
+void createMenu(MenuStruct menuStruct) {
+  ScreenInfo screenStruct;
 
-  MenuStruct items[maxItem];
-  Item itemList[maxItem];
+  screenStruct.alignmentX = menuStruct.menuItemAlignmentX;
+  screenStruct.alignmentY = menuStruct.menuItemAlignmentY;
+  screenStruct.colorRange = menuStruct.colorRange;
+  screenStruct.fontSize = menuStruct.fontSize;
+  screenStruct.name = menuStruct.name;
 
-  void createMenu(MenuStruct menuStruct) {
-    char keyCode;
-    int counter = 2;
+  char keyCode;
+  int counter = 2;
 
-    struct Item itemList[menuStruct.maxItem];
+  // struct Item itemList[menuStruct.maxItem];
 
-    for (int i = 0;;) {
-      modifyScreenContentPlacement(menuStruct);
+  for (int i = 0;;) {
+    modifyScreenContentPlacement(screenStruct);
 
-      /* Read a single character from the console without echoing the character
-       */
-      keyCode = _getch();
+    /* Read a single character from the console without echoing the character
+     */
+    keyCode = _getch();
 
-      if (keyCode == ESCAPE_KEY && (counter >= 2 && counter <= 3))
-        counter--;
+    if (keyCode == UP_ARROW_KEY && (counter >= 2 && counter <= 3))
+      counter--;
 
-      if (keyCode == DOWN_ARROW_KEY && (counter >= 1 && counter <= 2))
-        counter++;
+    if (keyCode == DOWN_ARROW_KEY && (counter >= 1 && counter <= 2))
+      counter++;
 
-      if (keyCode == ESCAPE_KEY)
-        exit(EXIT_FAILURE);
+    if (keyCode == ESCAPE_KEY)
+      exit(EXIT_FAILURE);
 
-      if (keyCode == CURRIAGE_RETURN) {
-        for (int i = 1; i < menuStruct.maxItem; i++) {
-          if (counter == itemList[i].id) {
-            itemList[i].action();
-          }
-        }
+    // if (keyCode == CURRIAGE_RETURN) {
+    //   for (int i = 1; i < menuStruct.maxItem; i++) {
+    //     if (counter == i) {
+    //       screenStruct.showMenu();
+    //     }
+    //   }
+    // }
+
+    if (keyCode == CURRIAGE_RETURN) {
+      // carriage return = enter key
+      if (counter == 1) {
+        displaySecondSemMenu();
       }
-
-      for (int i = 1; i < menuStruct.maxItem; i++) {
-        itemList[i].colorRange = 7; // default color white
-
-        if (counter == i) {
-          itemList[i].colorRange = 12; // color red
-        }
+      if (counter == 2) {
+        displayMidtermMenu();
+      }
+      if (counter == 3) {
+        displayFinalSemMenu();
       }
     }
 
-    for (int i = 0; i < this->maxItem; i++) {
-      this->items[i] = menuStruct;
+    for (int i = 1; i < menuStruct.maxItem; i++) {
+      screenStruct.colorRange = 7; // default color white
+
+      if (counter == i) {
+        screenStruct.colorRange = 12; // color red
+      }
     }
   }
 
-  void addItem(Item _item) {
-    Item item;
-
-    item.id = generateId();
-    item.text = _item.text;
-    item.action = _item.action;
-  }
-
-  void setName(string name) {}
-
-  void setAlignment(short x, short y) {
-    this->alignmentX = x;
-    this->alignmentY = y;
-  }
-
-  void setMaxItem(int maxItem) { this->maxItem = maxItem; }
-};
+  // for (int i = 0; i < this.maxItem; i++) {
+  //   this.items[i] = menuStruct;
+  // }
+}
 
 // void createMenu(MenuStruct menuStruct) {
 //   char keyCode;
