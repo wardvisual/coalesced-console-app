3549d98d199639ba62b6e7daca6caf87d53376d0
Edward <wardvisual@gmail.com>
2022-06-27 20:45:20 +0800

    [CHORE] mini chanages

---
+++ src/includes/structures/screenStruct.cpp
@@ -0,0 +1,13 @@
+// Defining a struct called ScreenInfo.
+
+#ifndef SCREEN_INFO
+#define SCREEN_INFO
+struct ScreenStruct {
+  int alignmentX;
+  int alignmentY;
+  int colorRange;
+  int fontSize;
+  string name;
+};
+#endif 
+
