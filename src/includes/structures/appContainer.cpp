5a65dc016acd53e8a516416328a92c5120242563
Edward <wardvisual@gmail.com>
2022-06-28 21:23:27 +0800

    [CHORE] mini changes

---
+++ src/includes/structures/appContainer.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+#include "../constants/maximumLength.cpp"
+
+#include "menuStruct.cpp"
+
+struct ApplicationContainer {
+  vector<MenuStruct> menuStruct;
+  //   struct MenuStruct menuStructContainer[MAX_ARRAY_LENGTH_LOW];
+}
