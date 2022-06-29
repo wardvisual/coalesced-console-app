96f6ee930c9ab9a1ecde4efbc7dcbb72c809b6c6
Edward <wardvisual@gmail.com>
2022-06-29 21:08:58 +0800

    [CHORE] mini changes

--- src/includes/structures/menuStruct.cpp
+++ src/includes/structures/menuStruct.cpp
@@ -2,12 +2,12 @@
 #define MENU_STRUCT
 
 struct MenuStruct {
-  int maxItem;
-  int menuItemAlignmentX;
-  int menuItemAlignmentY;
-  int colorRange;
+  int alignmentX;
+  int alignmentY;
+  int textColor;
   int fontSize;
   string name;
+  void execute(void callback()) { callback(); };
 };
 
 #endif
