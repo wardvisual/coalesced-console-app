584aabba4698ebe24734946358bd3a789a0d03d9
Edward <wardvisual@gmail.com>
2022-07-01 06:38:09 +0800

    checks whether the given token has been #defined earlier in the file or in an included file; if not, it includes the code between it and the closing #else or, if no #else is present, #endif statement.

--- src/includes/utilities/generateId.cpp
+++ src/includes/utilities/generateId.cpp
@@ -1,9 +1,12 @@
 #include <ctime>
 
-using namespace std;
+#ifndef CPP_GENERATE_ID
+#define CPP_GENERATE_ID
 
 int generateId() {
-    time_t seconds = time(0);
-    
-    return seconds;   
+  time_t seconds = time(0);
+
+  return seconds;
 }
+
+#endif
