4905c0069ce47991ddda7b1a10ca931c66205bc7
Edward <wardvisual@gmail.com>
2022-07-02 07:34:08 +0800

    [FEAT] create color method features

--- src/includes/utilities/color.cpp
+++ src/includes/utilities/color.cpp
@@ -1,24 +1,50 @@
 /* Including the library that is used in this utility. */
 #include "windows.h"
 
-#ifndef CPP_COLOR
-#define CPP_COLOR
+#include "../constants/color.cpp"
 
-/**
- * @brief Macro contants for colors
- */
-#define BLUE 1
-#define GREEN 2
-#define RED 4
-#define WHITE 15
+#ifndef CPP_COLOR_UTILITY
+#define CPP_COLOR_UTILITY
 
 /**
  * It sets the color of the text in the console
  *
  * @param color The color you want to apply.
  */
-void applyColor(int color) {
-  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+void applyTextColor(int range) {
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), range);
+}
+
+void applyBackgroundColor(int range) {
+  SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), range);
+}
+
+void clearScreen(int length) {
+  for (int i = 0; i < length; i++) {
+    std::cout << "\b";
+  }
+}
+
+void animateTextColor(std::string content) {
+  int colors[] = {TEXT_BLUE, TEXT_GREEN, TEXT_WHITE, TEXT_RED};
+  std::string textContent;
+  int i = 0;
+
+  while (true) {
+    Sleep(1000);
+
+    applyTextColor(colors[i]);
+    if (i >= 4)
+      i = 0;
+
+    applyTextColor(colors[i]);
+
+    std::cout << content;
+
+    clearScreen(content.length());
+
+    i++;
+  }
 }
 
 #endif
