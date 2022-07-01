da155d0a51276c1316bd4cbf0652d4b8fed07ef5
Edward <wardvisual@gmail.com>
2022-07-02 07:32:29 +0800

    [CHORE] create details about developers

---
+++ src/core/apps/information/index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void aboutUs() {
+  int MAX = 6;
+
+  string developers[MAX] = {
+    "Manlangit, Eduardo Jr.",
+    "Galindo, Mark Lim",
+    "Cortez, Fletcher",
+    "Santarin, Khenjie",
+    "Gagauiz, Angelica",
+    "Abdon, Williane Antonette"
+  }
+}
