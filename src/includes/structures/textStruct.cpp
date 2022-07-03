011cec11d5aa267e442fd359f5f47a99be406ce1
Edward <wardvisual@gmail.com>
2022-07-03 17:48:34 +0800

    [CHORE] create text structure

---
+++ src/includes/structures/textStruct.cpp
@@ -0,0 +1,13 @@
+
+#ifndef CPP_TEXT_STRUCT
+#define CPP_TEXT_STRUCT
+
+struct TextStruct {
+  std::string content;
+  int bgColor = 0;
+  int textColor = 0;
+  int alignmentX = 0;
+  int alignmentY = 0;
+};
+
+#endif
