ca6e659060c03901dbfe2689ddd78fe55c9fb727
Edward <wardvisual@gmail.com>
2022-07-03 22:29:01 +0800

    [CHORE] clean display menu component

--- src/core/apps/finals/menu/showMenu.cpp
+++ src/core/apps/finals/menu/showMenu.cpp
@@ -1,6 +1,20 @@
 #ifndef CPP_FINALS_MENU
 #define CPP_FINALS_MENU
 
-void displayFinalsMenu() { std::cout << "finals sem"; }
+#include "../../../../includes/constants/color.cpp"
+
+#include "../../../../includes/utilities/color.cpp"
+
+#include "../../../../includes/utilities/gotoxy.cpp"
+
+#include "../../../../includes/helpers/symbol.cpp"
+
+void displayFinalsMenu() {
+  std::string generatedSymbol87 = generateSymbol("-", 20);
+
+  applyTextColor(TEXT_WHITE);
+  alignContent(40, 10);
+  std::cout << "<< " << generatedSymbol87 << " >>";
+}
 
 #endif
