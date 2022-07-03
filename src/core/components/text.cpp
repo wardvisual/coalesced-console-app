03ef75c71bf826010af6500938262e8e89a7fc3d
Edward <wardvisual@gmail.com>
2022-07-03 22:30:25 +0800

    [CHORE] change to parameters

--- src/core/components/text.cpp
+++ src/core/components/text.cpp
@@ -7,16 +7,10 @@
 /* Including the files in the utilities folder. */
 #include "../../includes/utilities/gotoxy.cpp"
 
-void text(std::string content, int textColor) {
-  std::cout << 1;
-  // if (property.bgColor)
-  //   applyBackgroundColor(property.bgColor);
-
-  // if (property.textColor)
-  //   applyTextColor(property.textColor);
-
-  // alignContent(property.alignmentX, property.alignmentY);
-  // std::cout << property.content;
+void text(std::string content, int textColor, int alignmentX, int alignmentY) {
+  applyTextColor(textColor);
+  alignContent(alignmentX, alignmentY);
+  std::cout << content;
 }
 
 #endif
