04c8c21e5a1bfda4c57254b4969489c9dbf895d1
Edward <wardvisual@gmail.com>
2022-07-03 22:29:14 +0800

    [CHORE] clean display menu component

--- src/core/apps/midterm/menu/showMenu.cpp
+++ src/core/apps/midterm/menu/showMenu.cpp
@@ -1,6 +1,20 @@
 #ifndef CPP_MIDTERM_MENU
 #define CPP_MIDTERM_MENU
 
-void displayMidtermMenu() { std::cout << "midterm sem"; }
+#include "../../../../includes/constants/color.cpp"
+
+#include "../../../../includes/utilities/color.cpp"
+
+#include "../../../../includes/utilities/gotoxy.cpp"
+
+#include "../../../../includes/helpers/symbol.cpp"
+
+void displayMidtermMenu() {
+  std::string generatedSymbol87 = generateSymbol("-", 20);
+
+  applyTextColor(TEXT_WHITE);
+  alignContent(40, 10);
+  std::cout << " <<  " << generatedSymbol87 << " >> ";
+}
 
 #endif
