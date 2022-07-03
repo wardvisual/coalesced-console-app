10a4a7b816669e0c9cf47341fe247eb62887f5aa
Edward <wardvisual@gmail.com>
2022-07-03 22:31:10 +0800

    [CHORE] clean show menu

---
+++ src/core/apps/information/menu/showMenu.cpp
@@ -0,0 +1,33 @@
+#ifndef CPP_INFORMATION_MENU
+#define CPP_INFORMATION_MENU
+
+#include "../../../../includes/constants/color.cpp"
+
+#include "../../../../includes/utilities/color.cpp"
+
+#include "../../../../includes/utilities/gotoxy.cpp"
+
+#include "../../../../includes/helpers/symbol.cpp"
+
+void displayAboutUsMenu() {
+  //   std::string generatedSymbol87 = generateSymbol("-", 20);
+
+  //   applyTextColor(TEXT_WHITE);
+  //   alignContent(40, 10);
+  //   std::cout << " << " << generatedSymbol87 << " >> ";
+
+  int MAX = 6;
+
+  std::string developers[MAX] = {"\t\t\t\t\tManlangit, Eduardo Jr.\n",
+                                 "\t\t\t\t\t\tGalindo, Mark Lim.\n",
+                                 "\t\t\t\t\t\tCortez, Fletcher.\n",
+                                 "\t\t\t\t\t\tSantarin, Khenjie.\n",
+                                 "\t\t\t\t\t\tGagauiz, Angelica.\n",
+                                 "\t\t\t\t\t\tAbdon, Williane Antonette.\n"};
+
+  for (int i = 0; i < MAX; i++) {
+    std::cout << developers[i] << "\n";
+  }
+}
+
+#endif
