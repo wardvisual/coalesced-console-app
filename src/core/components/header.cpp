c422c5f9a9e1c9b6569318154e53a5b8d0a19d4a
Edward <wardvisual@gmail.com>
2022-07-03 17:51:30 +0800

    [CHORE] create header component

---
+++ src/core/components/header.cpp
@@ -0,0 +1,43 @@
+#ifndef CPP_HEADER_COMPONENT
+#define CPP_HEADER_COMPONENT
+
+/* Including the files in the utilities folder. */
+#include "../../includes/utilities/color.cpp"
+#include "../../includes/utilities/gotoxy.cpp"
+
+/* Including the files in the helpers folder. */
+#include "../../includes/helpers/symbol.cpp"
+
+/* Including the files in the constants folder. */
+#include "../../includes/constants/color.cpp"
+
+void headerComponent(std::string title) {
+  std::string generatedSymbol18 = generateSymbol("-", 18);
+  std::string generatedSymbol91 = generateSymbol(" ", 91);
+  std::string generatedSymbol87 = generateSymbol("-", 87);
+
+  applyBackgroundColor(BG_BLUE);
+  alignContent(10, 3);
+  std::cout << generatedSymbol91;
+
+  applyTextColor(TEXT_BLUE);
+  alignContent(4, 4);
+  std::cout << "|| >>>> " << generatedSymbol87 << " <<<< || ";
+
+  applyTextColor(TEXT_WHITE);
+  alignContent(8, 5);
+  std::cout << " >>>> " + generatedSymbol18 + title + generatedSymbol18 +
+                   " <<<<< ";
+
+  applyTextColor(TEXT_BLUE);
+  alignContent(4, 6);
+  std::cout << "|| >>>> " << generatedSymbol87 << " <<<< || ";
+
+  applyBackgroundColor(BG_BLUE);
+  alignContent(10, 7);
+  std::cout << generatedSymbol91;
+
+  std::cout << "\n\n";
+}
+
+#endif
