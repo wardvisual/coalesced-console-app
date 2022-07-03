f0020e970c2147ec52287d3b08ea2bbf15eaef69
Edward <wardvisual@gmail.com>
2022-07-03 17:51:42 +0800

    [CHORE] clean folder naming

--- src/application.cpp
+++ src/application.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 
 /* Including the files in the component folder. */
-#include "./core/component/header.cpp"
-#include "./core/component/menu.cpp"
-#include "./core/component/newLine.cpp"
+#include "./core/components/header.cpp"
+#include "./core/components/menu.cpp"
+#include "./core/components/newLine.cpp"
 
 void renderConsoleApplication() {
-  const int standardSpace = 3;
 
   headerComponent("GROUP SIX FINAL PROJECT | CONSOLE APPLICATION");
 
