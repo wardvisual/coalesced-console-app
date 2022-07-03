9b0c71ba40a4145191d7ed87d0450923f42b1ee1
Edward <wardvisual@gmail.com>
2022-07-03 22:29:34 +0800

    [CHORE] clean display menu component

--- src/core/apps/seconsem/menu/showMenu.cpp
+++ src/core/apps/seconsem/menu/showMenu.cpp
@@ -2,6 +2,20 @@
 #ifndef CPP_SECOND_SEM_MENU
 #define CPP_SECOND_SEM_MENU
 
-void displaySecondSemMenu() { std::cout << "second sem"; }
+#include "../../../../includes/constants/color.cpp"
+
+#include "../../../../includes/utilities/color.cpp"
+
+#include "../../../../includes/utilities/gotoxy.cpp"
+
+#include "../../../../includes/helpers/symbol.cpp"
+
+void displaySecondSemMenu() {
+  std::string generatedSymbol87 = generateSymbol("-", 20);
+
+  applyTextColor(TEXT_WHITE);
+  alignContent(40, 10);
+  std::cout << " <<  " << generatedSymbol87 << " >> ";
+}
 
 #endif
