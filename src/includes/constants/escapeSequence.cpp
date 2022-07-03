0cf3f0f0012f705bafddfd65cdc1724c7f98a560
Edward <wardvisual@gmail.com>
2022-07-03 17:49:20 +0800

    [CHORE] update curriage return description

--- src/includes/constants/escapeSequence.cpp
+++ src/includes/constants/escapeSequence.cpp
@@ -1,9 +1,11 @@
 #ifndef CPP_ESCAPE_SEQUENCE_CONSTANT
 #define CPP_ESCAPE_SEQUENCE_CONSTANT
 
-
 /* Moves the active position to the initial position of the current line. */
+/* Carriage return means to return to the beginning of the current line without
+  advancing downward. The name comes from a printer's carriage, as monitors
+  were rare when the name was coined. This is commonly escaped as \r ,
+  abbreviated CR, and has ASCII value 13 or 0x0D . */
 #define CURRIAGE_RETURN 13
-
 
 #endif
