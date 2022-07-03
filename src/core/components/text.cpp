f60e7ff83c5b6acdc3152a3d4dafe253bba40265
Edward <wardvisual@gmail.com>
2022-07-03 17:49:34 +0800

    [CHORE] create text component

---
+++ src/core/components/text.cpp
@@ -0,0 +1,22 @@
+#ifndef CPP_TITLE_COMPONENT
+#define CPP_TITLE_COMPONENT
+
+/* Including the files in the structures folder. */
+#include "../../includes/structures/textStruct.cpp"
+
+/* Including the files in the utilities folder. */
+#include "../../includes/utilities/gotoxy.cpp"
+
+void text(std::string content, int textColor) {
+  std::cout << 1;
+  // if (property.bgColor)
+  //   applyBackgroundColor(property.bgColor);
+
+  // if (property.textColor)
+  //   applyTextColor(property.textColor);
+
+  // alignContent(property.alignmentX, property.alignmentY);
+  // std::cout << property.content;
+}
+
+#endif
