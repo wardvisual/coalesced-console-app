9058f3cafc7a60b5396a54cbb3b9de90eea04bab
Edward <wardvisual@gmail.com>
2022-07-03 17:51:15 +0800

    [CHORE] update folder of these files header.cpp, menu.cpp, newLine.cpp and text.cpp

--- src/core/component/newLine.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef CPP_NEW_LINE_COMPONENT
-#define CPP_NEW_LINE_COMPONENT
-
-#include "../../includes/helpers/symbol.cpp"
-
-void newLine(int count) { std::cout << generateSymbol("\n", count); }
-
-#endif
