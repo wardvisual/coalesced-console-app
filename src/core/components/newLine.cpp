f8cbcd9125e91f22912e178a630254e3cebfde6a
Edward <wardvisual@gmail.com>
2022-07-03 17:50:06 +0800

    [CHORE] create newLine component to simplify adding new line every single line of a code

---
+++ src/core/components/newLine.cpp
@@ -0,0 +1,8 @@
+#ifndef CPP_NEW_LINE_COMPONENT
+#define CPP_NEW_LINE_COMPONENT
+
+#include "../../includes/helpers/symbol.cpp"
+
+void newLine(int count) { std::cout << generateSymbol("\n", count); }
+
+#endif
