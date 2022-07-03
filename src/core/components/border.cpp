b065d4218205cc77c620df7d7f103bce1a3fd4c0
Edward <wardvisual@gmail.com>
2022-07-03 22:32:07 +0800

    [CHORE] create border component

---
+++ src/core/components/border.cpp
@@ -0,0 +1,9 @@
+#ifndef CPP_BORDER_COMPONENT
+#define CPP_BORDER_COMPONENT
+
+#include "../../includes/structures/borderStruct.cpp"
+#include "../../includes/utilities/gotoxy.cpp"
+
+void border(BorderStruct border) {}
+
+#endif
