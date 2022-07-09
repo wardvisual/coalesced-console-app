a7be126a97db272be2dea94898ddfa10b5ac2899
Edward <wardvisual@gmail.com>
2022-07-10 00:12:06 +0800

    chore: update texts layout

--- src/core/apps/information/menu/showMenu.cpp
+++ src/core/apps/information/menu/showMenu.cpp
@@ -2,32 +2,54 @@
 #define CPP_INFORMATION_MENU
 
 #include "../../../../includes/constants/color.cpp"
-
 #include "../../../../includes/helpers/color.cpp"
-
 #include "../../../../includes/helpers/gotoxy.cpp"
-
 #include "../../../../includes/helpers/symbol.cpp"
 
+#include "../../../components/text.cpp"
+
 void displayAboutUsMenu() {
-  //   std::string generatedSymbol87 = generateSymbol("-", 20);
-
-  //   applyTextColor(TEXT_WHITE);
-  //   alignContent(40, 10);
-  //   std::cout << " << " << generatedSymbol87 << " >> ";
-
   int MAX = 6;
+  std::string developers[MAX] = {
+      "Manlangit, Eduardo Jr.\n", "Galindo, Mark Lim.\n",
+      "Cortez, Fletcher.\n",      "Santarin, Khenjie.\n",
+      "Gagauiz, Angelica.\n",     "Abdon, Williane Antonette.\n"};
 
-  std::string developers[MAX] = {"\t\t\t\t\tManlangit, Eduardo Jr.\n",
-                                 "\t\t\t\t\t\tGalindo, Mark Lim.\n",
-                                 "\t\t\t\t\t\tCortez, Fletcher.\n",
-                                 "\t\t\t\t\t\tSantarin, Khenjie.\n",
-                                 "\t\t\t\t\t\tGagauiz, Angelica.\n",
-                                 "\t\t\t\t\t\tAbdon, Williane Antonette.\n"};
+  /* Text Heading. */
+  text("Description:", TEXT_BLUE, 34, 10);
+  text("Programming Language(s) and Tools:", TEXT_BLUE, 34, 20);
+  text("Our Development Process:", TEXT_BLUE, 34, 25);
+  text("Developers:", TEXT_BLUE, 100, 10);
+  text("Professor:", TEXT_BLUE, 100, 25);
 
+  /* Text Paragraph. */
+  text("A project submitted for fulfillment for the final semester", TEXT_WHITE,
+       34, 12);
+  text("of the first year of computer science at the Eulogio Amang", TEXT_WHITE,
+       34, 13);
+  text("Rodriguez Institute of Science and Technology General Mariano",
+       TEXT_WHITE, 34, 14);
+  text("Alvarez, Cavite. We proudly present this wonderful and", TEXT_WHITE, 34,
+       15);
+  text("interactive console-based application made in the C++", TEXT_WHITE, 34,
+       16);
+  text("programming language.", TEXT_WHITE, 34, 17);
+  text("C++ Programming Language, Visual Studio Code/C++ IDEs.", TEXT_WHITE, 34,
+       22);
+  text("Planning, Implementation/Development, Code Refactoring.", TEXT_WHITE,
+       34, 27);
+  text("Mrs. Catherine S. Dumpit.", TEXT_WHITE, 100, 27);
+
+  /* Displaying all the members of the project */
   for (int i = 0; i < MAX; i++) {
-    std::cout << developers[i] << "\n";
+    text(developers[i] + "\n", TEXT_WHITE, 100, 12 + i + i);
   }
 }
 
 #endif
+
+// A project submitted for fulfillment for the final semester of the first year
+// of computer science at the Eulogio Amang Rodriguez Institute of Science and
+// Technology General Mariano Alvarez, Cavite. We proudly present this wonderful
+// and interactive console-based application made in the C++ programming
+// language.
