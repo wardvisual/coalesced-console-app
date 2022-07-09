16e2c978de7d4566d9f3fb4c9a777fe59787cef0
Edward <wardvisual@gmail.com>
2022-07-10 00:12:49 +0800

    chore: update header placement

--- src/core/components/header.cpp
+++ src/core/components/header.cpp
@@ -12,27 +12,27 @@
 void headerComponent(std::string title) {
   std::string generatedSymbol18 = generateSymbol("-", 18);
   std::string generatedSymbol91 = generateSymbol(" ", 91);
-  std::string generatedSymbol87 = generateSymbol("-", 87);
+  std::string generatedSymbol80 = generateSymbol("-", 80);
 
   applyBackgroundColor(BG_BLUE);
-  alignContent(10, 3);
+  alignContent(34, 3);
   std::cout << generatedSymbol91;
 
   applyTextColor(TEXT_BLUE);
-  alignContent(4, 4);
-  std::cout << "|| >>>> " << generatedSymbol87 << " <<<< || ";
+  alignContent(32, 4);
+  std::cout << "|| >>>> " << generatedSymbol80 << " <<<< || ";
 
   applyTextColor(TEXT_WHITE);
-  alignContent(8, 5);
+  alignContent(33, 5);
   std::cout << " >>>> " + generatedSymbol18 + title + generatedSymbol18 +
                    " <<<<< ";
 
   applyTextColor(TEXT_BLUE);
-  alignContent(4, 6);
-  std::cout << "|| >>>> " << generatedSymbol87 << " <<<< || ";
+  alignContent(32, 6);
+  std::cout << "|| >>>> " << generatedSymbol80 << " <<<< || ";
 
   applyBackgroundColor(BG_BLUE);
-  alignContent(10, 7);
+  alignContent(34, 7);
   std::cout << generatedSymbol91;
 
   std::cout << "\n\n";
