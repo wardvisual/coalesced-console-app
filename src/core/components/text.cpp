604bad129c64ec01d7862a10a1a14d97f6c13ce2
Edward <wardvisual@gmail.com>
2022-07-10 13:30:00 +0800

    chore: add comment description

--- src/core/components/text.cpp
+++ src/core/components/text.cpp
@@ -1,7 +1,14 @@
+/**
+ * @file /src/core/components/text.cpp
+ * @brief A component that is used to create a complete text style.
+ *
+ * */
+
 #ifndef CPP_TITLE_COMPONENT
 #define CPP_TITLE_COMPONENT
 
-/* Including the files in the utilities folder. */
+/* Including the files in the helpers folder. */
+#include "../../includes/helpers/color.cpp"
 #include "../../includes/helpers/gotoxy.cpp"
 
 void text(std::string content, int textColor, int alignmentX, int alignmentY) {
