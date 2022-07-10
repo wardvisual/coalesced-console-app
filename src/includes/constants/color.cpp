5150c45417d9083a769dcd964ffc7ba7a96d56fc
Edward <wardvisual@gmail.com>
2022-07-10 13:32:03 +0800

    chore: add constants for colors

--- src/includes/constants/color.cpp
+++ src/includes/constants/color.cpp
@@ -1,11 +1,31 @@
+/**
+ * @file /src/includes/constants/color.cpp
+ * @brief A macro constant that is used to supply a color value.
+ *
+ * */
+
 #ifndef CPP_COLOR_CONSTANT
 #define CPP_COLOR_CONSTANT
 
 #define BG_BLUE 30
+#define BG_LIGHT_GREEN 50
+
 #define TEXT_BLUE 1
 #define TEXT_GREEN 2
+#define TEXT_AQUA 3
 #define TEXT_RED 4
-#define TEXT_WHITE 15
+#define TEXT_PURPLE 5
+#define TEXT_YELLOW 6
+#define TEXT_WHITE 7
+#define TEXT_GRAY 8
+#define TEXT_LIGHT_BLUE 9
+#define TEXT_BLACK 0
+#define TEXT_LIGHT_GREEN 10
+#define TEXT_LIGHT_AQUA 11
+#define TEXT_LIGHT_rED 12
+#define TEXT_LIGHT_PURPLE 13
+#define TEXT_LIGHT_YELLOW 14
+#define TEXT_BRIGHT_wHITE 15
 
 // #define BACKGROUND_GREEN 40
 // #define BACKGROUND_RED 50
