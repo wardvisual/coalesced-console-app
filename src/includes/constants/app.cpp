d2bd5e09654f078bacc1f9a9efe95e02d60dc91d
Edward <wardvisual@gmail.com>
2022-07-10 13:32:21 +0800

    chore: add comment description

--- src/includes/constants/app.cpp
+++ src/includes/constants/app.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file /src/includes/constants/app.cpp
+ * @brief A macro constant that is used to identify what type of application we
+ * are going to execute. This file is related to /src/core/apps/*.cpp
+ */
+
 #ifndef CPP_APPS_CONSTANT
 #define CPP_APPS_CONSTANT
 
