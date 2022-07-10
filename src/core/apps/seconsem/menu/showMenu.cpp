0a7e7c7c8ca73ec7df16fef4404acc92f62d7df7
Edward <wardvisual@gmail.com>
2022-07-10 13:33:13 +0800

    chore: add second sem menu UI

--- src/core/apps/seconsem/menu/showMenu.cpp
+++ src/core/apps/seconsem/menu/showMenu.cpp
@@ -2,20 +2,23 @@
 #ifndef CPP_SECOND_SEM_MENU
 #define CPP_SECOND_SEM_MENU
 
+#include "../../../../includes/constants/alignment.cpp"
 #include "../../../../includes/constants/color.cpp"
 
 #include "../../../../includes/helpers/color.cpp"
-
 #include "../../../../includes/helpers/gotoxy.cpp"
 
-#include "../../../../includes/helpers/symbol.cpp"
+#include "../../../components/text.cpp"
 
 void displaySecondSemMenu() {
-  std::string generatedSymbol87 = generateSymbol("-", 20);
 
-  applyTextColor(TEXT_WHITE);
-  alignContent(40, 10);
-  std::cout << " <<  " << generatedSymbol87 << " >> ";
+  text("Second Sem Applications", TEXT_BLUE, ALIGNMENTX38, ALIGNMENTY10);
+
+  text("Activity 01", TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY12);
+
+  text("Activity 02", TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY14);
+
+  text("Activity 03", TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY16);
 }
 
 #endif
