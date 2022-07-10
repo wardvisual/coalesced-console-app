7b91e047d2174c9ee3c3dc6c2b84af891de4157b
Edward <wardvisual@gmail.com>
2022-07-10 08:25:45 +0800

    chore: remove uncessary library

--- src/application.cpp
+++ src/application.cpp
@@ -3,10 +3,8 @@
 /* Including the files in the component folder. */
 #include "./core/components/header.cpp"
 #include "./core/components/menu.cpp"
-#include "./core/components/newLine.cpp"
 
 void renderConsoleApplication() {
-
   headerComponent("GROUP SIX FINAL PROJECT | CONSOLE APPLICATION");
 
   menuComponent();
