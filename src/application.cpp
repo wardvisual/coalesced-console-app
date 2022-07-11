ed9c433decef2f25c2135c5ac8f069e6078e40d7
Edward <wardvisual@gmail.com>
2022-07-11 21:14:56 +0800

    chore: rename header title

--- src/application.cpp
+++ src/application.cpp
@@ -1,11 +1,18 @@
+/**
+ * @file /src/application.cpp
+ * @brief sub directory
+ *
+ */
+
 #include <iostream>
+#include <windows.h>
 
 /* Including the files in the component folder. */
 #include "./core/components/header.cpp"
 #include "./core/components/menu.cpp"
 
 void renderConsoleApplication() {
-  headerComponent("GROUP SIX FINAL PROJECT | CONSOLE APPLICATION");
+  headerComponent(" GROUP SIX FINAL PROJECT | CONSOLE APPLICATION ");
 
   menuComponent();
 }
