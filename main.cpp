d23e86a2e75b04f3b624f556b8e1000435172379
Edward <wardvisual@gmail.com>
2022-07-11 10:14:22 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/**
+ * @file main.cpp
+ * @brief This file is an entry for this entire application.
+ *
+ */
 
 /* Including the application.cpp file in the main.cpp file. */
 #include "./src/application.cpp"
@@ -5,5 +10,9 @@
 /**
  * It renders a consoleAapplication function
  */
+
+void newFunction() { std::cout << "new function"; }
+// new comment
+// 10:11 AM
 
 int main() { renderConsoleApplication(); }
