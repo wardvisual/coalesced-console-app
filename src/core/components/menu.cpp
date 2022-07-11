f2ee026df03b060efb9b164aaed6ca7f88589ef4
Edward <wardvisual@gmail.com>
2022-07-11 21:12:34 +0800

    chore: realign menu item coordinates

--- src/core/components/menu.cpp
+++ src/core/components/menu.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file /src/core/components/menu.cpp
+ * @brief A component that is used to create a complete menu.
+ *
+ * */
+
 #ifndef CPP_MENU_COMPONENT
 #define CPP_MENU_COMPONENT
 
@@ -14,6 +20,7 @@
 #include "../../includes/constants/app.cpp"
 #include "../../includes/constants/color.cpp"
 
+#include "header.cpp"
 #include "text.cpp"
 
 void menuComponent() {
@@ -24,39 +31,38 @@ void menuComponent() {
   MenuStruct developersInformation, secondSemMenu, midtermMenu, finalSemMenu,
       supplementaryMenu;
 
-  text("Main Menu", TEXT_BLUE, ALIGNMENTX2, ALIGNMENTY5);
-
+  mainMenuHeaderComponent(" MAIN MENU ");
   /* Assigning values to the secondSemMenu struct instance. */
   developersInformation.name = "01. About the Project";
   developersInformation.textColor = TEXT_WHITE;
   developersInformation.alignmentX = ALIGNMENTX2;
-  developersInformation.alignmentY = ALIGNMENTY8;
+  developersInformation.alignmentY = ALIGNMENTY10;
   developersInformation.type = DEVELOPERS_INFORMATION;
 
   secondSemMenu.name = "02. Second Sem Applications";
   secondSemMenu.textColor = TEXT_WHITE;
   secondSemMenu.alignmentX = ALIGNMENTX2;
-  secondSemMenu.alignmentY = ALIGNMENTY10;
+  secondSemMenu.alignmentY = ALIGNMENTY12;
   secondSemMenu.type = SECOND_SEM;
 
   /* Assigning values to the  midtermMenu struct instance. */
   midtermMenu.name = "03. Midterm Applications";
   midtermMenu.textColor = TEXT_WHITE;
   midtermMenu.alignmentX = ALIGNMENTX2;
-  midtermMenu.alignmentY = ALIGNMENTX12;
+  midtermMenu.alignmentY = ALIGNMENTX14;
   midtermMenu.type = MID_TERM;
 
   /* Assigning values to the  finalSemMenu struct instance. */
   finalSemMenu.name = "04. Final Sem Applications";
   finalSemMenu.textColor = TEXT_WHITE;
   finalSemMenu.alignmentX = ALIGNMENTX2;
-  finalSemMenu.alignmentY = ALIGNMENTX14;
+  finalSemMenu.alignmentY = ALIGNMENTX16;
   finalSemMenu.type = FINAL_SEM;
 
   supplementaryMenu.name = "05. Supplementary Applications";
   supplementaryMenu.textColor = TEXT_WHITE;
   supplementaryMenu.alignmentX = ALIGNMENTX2;
-  supplementaryMenu.alignmentY = ALIGNMENTX16;
+  supplementaryMenu.alignmentY = ALIGNMENTX18;
   supplementaryMenu.type = SUMPPLEMENTARY_APP;
 
   /* Creating an array of MenuStruct. */
