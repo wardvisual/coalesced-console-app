4b21e824688aea1f3bfa56b14c5f331f9f9ba16b
Edward <wardvisual@gmail.com>
2022-07-11 21:13:43 +0800

    chore: create meain menu header

--- src/core/components/header.cpp
+++ src/core/components/header.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file /src/core/components/header.cpp
+ * @brief A component that is used to create a complete header style.
+ *
+ * */
+
 #ifndef CPP_HEADER_COMPONENT
 #define CPP_HEADER_COMPONENT
 
@@ -15,20 +21,43 @@
 void headerComponent(std::string title) {
   std::string generatedSymbol18 = generateSymbol("-", 18);
   std::string generatedSymbol91 = generateSymbol(" ", 91);
-  std::string generatedSymbol80 = generateSymbol("-", 80);
+  std::string generatedSymbol83 = generateSymbol("-", 83);
 
-  text(generatedSymbol91, BG_BLUE, ALIGNMENTX34, ALIGNMENTY3);
+  text(generatedSymbol91, BG_BLUE, ALIGNMENTX38, ALIGNMENTY3);
 
-  text("|| >>>> " + generatedSymbol80 + " <<<< || ", TEXT_BLUE, ALIGNMENTX32,
+  text(">>> " + generatedSymbol83 + " <<< ", TEXT_BLUE, ALIGNMENTX38,
        ALIGNMENTY4);
 
-  text(" >>>> " + generatedSymbol18 + title + generatedSymbol18 + " <<<<< ",
-       TEXT_WHITE, ALIGNMENTX33, ALIGNMENTY5);
+  text(" >>> " + generatedSymbol18 + title + generatedSymbol18 + " <<< ",
+       TEXT_WHITE, ALIGNMENTX37, ALIGNMENTY5);
 
-  text("|| >>>> " + generatedSymbol80 + " <<<< || ", TEXT_BLUE, ALIGNMENTX32,
+  text(">>> " + generatedSymbol83 + " <<< ", TEXT_BLUE, ALIGNMENTX38,
        ALIGNMENTY6);
 
-  text(generatedSymbol91, BG_BLUE, ALIGNMENTX34, ALIGNMENTY7);
+  text(generatedSymbol91, BG_BLUE, ALIGNMENTX38, ALIGNMENTY7);
+
+  std::cout << "\n\n";
+}
+
+void mainMenuHeaderComponent(std::string title) {
+  std::string generatedSymbol5 = generateSymbol("-", 5);
+  std::string generatedSymbol23 = generateSymbol("-", 23);
+  std::string generatedSymbol31 = generateSymbol(" ", 31);
+
+  text(generatedSymbol31, BG_WHITE, ALIGNMENTX2, ALIGNMENTY3);
+
+  text("|| " + generatedSymbol23 + " || ", TEXT_WHITE, ALIGNMENTX3,
+       ALIGNMENTY4);
+
+  text(" " + generatedSymbol5 + title + generatedSymbol5 + " ", TEXT_WHITE,
+       ALIGNMENTX7, ALIGNMENTY5);
+
+  text("|| " + generatedSymbol23 + " || ", TEXT_WHITE, ALIGNMENTX3,
+       ALIGNMENTY6);
+
+  text(generatedSymbol31, BG_WHITE, ALIGNMENTX2, ALIGNMENTY7);
+
+  //   text(generateSymbol("\n ", 18), BG_WHITE, ALIGNMENTX30, ALIGNMENTY2);
 
   std::cout << "\n\n";
 }
