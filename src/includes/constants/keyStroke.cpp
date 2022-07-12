9eec364dc2c76084dfc4364e99f03cb8f020764b
Edward <wardvisual@gmail.com>
2022-07-12 15:43:22 +0800

    chore: rename file

---
+++ src/includes/constants/keyStroke.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file /src/includes/constants/keyCode.cpp
+ * @brief A macro constant that is used to determine what key code is on the
+ * keyboard.
+ *
+ * */
+
+#ifndef CPP_KEYCODE_CONSTANT
+#define CPP_KEYCODE_CONSTANT
+
+/* Defining the key codes for the arrow keys and the escape key. */
+#define ESCAPE_KEY 27
+#define UP_ARROW_KEY 72
+#define DOWN_ARROW_KEY 80
+#define LEFT_ARROW_KEY 75
+#define RIGHT_ARROW_KEY 77
+
+#endif
