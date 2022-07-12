162a5545ace4a57092bf2f1a4e15d753f265787c
Edward <wardvisual@gmail.com>
2022-07-13 01:12:17 +0800

    chore: create border for input

--- src/core/components/input.cpp
+++ src/core/components/input.cpp
@@ -16,19 +16,24 @@
 #include <limits>
 
 template <typename T> void input(std::string label, T &referenceValue) {
-  int maxInputLength = 1;
+  std::string generatedSymbol31 = generateSymbol("-", 31);
 
-  text(label + ": ", TEXT_WHITE, ALIGNMENTX2, ALIGNMENTY22);
+  int maxInputLength = 1;
+  std::string errorMessage = "Invalid Input. Please try again!";
+
+  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2, ALIGNMENTY23);
+  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2, ALIGNMENTY25);
+
+  text(label + ": ", TEXT_WHITE, ALIGNMENTX2, ALIGNMENTY24);
   std::cin >> std::setw(maxInputLength) >> referenceValue;
 
   while (!std::cin.good()) {
-    text("\nInvalid Input. Please try again!", TEXT_LIGHT_RED, ALIGNMENTX2,
-         ALIGNMENTY23);
+    text(errorMessage, TEXT_LIGHT_RED, ALIGNMENTX2, ALIGNMENTY26);
 
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-    text(label + ": ", TEXT_WHITE, ALIGNMENTX2, ALIGNMENTY22);
+    text(label + ": ", TEXT_WHITE, ALIGNMENTX2, ALIGNMENTY24);
     std::cin >> std::setw(maxInputLength) >> referenceValue;
   }
 
