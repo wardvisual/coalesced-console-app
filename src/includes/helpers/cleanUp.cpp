852611ee9232f09c38313f8759b3a1b153dc70ed
Edward <wardvisual@gmail.com>
2022-07-13 18:21:35 +0800

    chore: add comment for clean up function

--- src/includes/helpers/cleanUp.cpp
+++ src/includes/helpers/cleanUp.cpp
@@ -7,6 +7,17 @@
 #ifndef CPP_CLEAN_UP_HELPER
 #define CPP_CLEAN_UP_HELPER
 
+/**
+ * It takes three functions as arguments and calls them in the order they were
+ * passed in
+ *
+ * @param retainComponenOne This is the function that will be called to retain
+ * the first component.
+ * @param retainComponenTwo This is the function that will be called to retain
+ * the second component of the screen.
+ * @param retainComponenThree This is the last component to be displayed on the
+ * screen.
+ */
 void cleanUpScreen(void (&retainComponenOne)(), void (&retainComponenTwo)()) {
   system("clear");
 
