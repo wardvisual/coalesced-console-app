83d25716e438d783e2c9b3682ba224f601d2cafd
Edward <wardvisual@gmail.com>
2022-07-13 16:44:34 +0800

    chore: seperate menu logic from menu component

--- src/core/components/menu.cpp
+++ src/core/components/menu.cpp
@@ -1,100 +1,46 @@
 /**
  * @file /src/core/components/menu.cpp
- * @brief A component that is used to create a complete menu.
- *
+ * @brief A component that is used to create a menu component
  * */
 
 #ifndef CPP_MENU_COMPONENT
 #define CPP_MENU_COMPONENT
 
-/* Including the files in the structures folder. */
-#include "../../includes/structures/menuStruct.cpp"
-
-/* Including the files in the helpers folder. */
-#include "../../includes/helpers/cleanUp.cpp"
-#include "../../includes/helpers/color.cpp"
-#include "../../includes/helpers/gotoxy.cpp"
-#include "../../includes/helpers/menu.cpp"
-
 /* Including the files in the constants folder. */
+#include "../../includes/constants/menu.cpp"
 #include "../../includes/constants/alignment.cpp"
-#include "../../includes/constants/app.cpp"
 #include "../../includes/constants/color.cpp"
 
-#include "../../core/apps/information/menu/showMenu.cpp"
-#include "../../core/apps/midterm/menu/showMenu.cpp"
-#include "../../core/apps/secondsem/menu/showMenu.cpp"
-
-#include "header.cpp"
-#include "input.cpp"
 #include "text.cpp"
 
-void menuItems(int selected);
-
-void menuComponent() {
-  const int maxMenuItem = 6;
-  int userInput, keyStroke = 0;
-
-  mainMenuHeaderComponent(" MAIN MENU ");
-  menuItems(0);
-
-  do {
-    input<int>("Input", userInput);
-
-    keyStroke = _getch();
-
-    if (userInput == 1) {
-      cleanUpScreen();
-      menuItems(1);
-      displayAboutUsMenu();
-    }
-
-    if (userInput == 2) {
-      cleanUpScreen();
-      menuItems(2);
-      displaySecondSemMenu();
-    }
-
-    if (userInput == 3) {
-      cleanUpScreen();
-      menuItems(3);
-      displayMidtermMenu();
-    }
-
-    if (userInput == 6) {
-      keyStroke = ESCAPE_KEY;
-      menuItems(6);
-
-      text("Thank your for using this application!\n\n", TEXT_BLUE, ALIGNMENTX2,
-           ALIGNMENTY24);
-    }
-  } while (keyStroke != ESCAPE_KEY);
-}
-
-void menuItems(int selected) {
-  int itemOne = 1, itemTwo = 2, itemThree = 3, itemFour = 4, itemFive = 5,
-      itemSix = 6;
-
-  text("01. About the Project", selected == itemOne ? TEXT_BLUE : TEXT_WHITE,
-       ALIGNMENTX2, ALIGNMENTY10);
+/**
+ * It prints the menu items on the screen
+ *
+ * @param selectedMenuItem The menu item that is currently selected.
+ */
+void menuComponent(int selectedMenuItem) {
+  text("01. About the Project",
+       selectedMenuItem == MENU_ITEM_ONE ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+       ALIGNMENTY10);
 
   text("02. Second Sem Applications",
-       selected == itemTwo ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2, ALIGNMENTY12);
+       selectedMenuItem == MENU_ITEM_TWO ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+       ALIGNMENTY12);
 
   text("03. Midterm Applications",
-       selected == itemThree ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
-       ALIGNMENTY14);
+       selectedMenuItem == MENU_ITEM_THREE ? TEXT_BLUE : TEXT_WHITE,
+       ALIGNMENTX2, ALIGNMENTY14);
 
   text("04. Final Sem Applications",
-       selected == itemFour ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+       selectedMenuItem == MENU_ITEM_FOUR ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
        ALIGNMENTY16);
 
   text("05. Supplementary Applications",
-       selected == itemFive ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+       selectedMenuItem == MENU_ITEM_FIVE ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
        ALIGNMENTY18);
 
-  text("06. Exit", selected == itemSix ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
-       ALIGNMENTY20);
+  text("06. Exit", selectedMenuItem == MENU_ITEM_SIX ? TEXT_BLUE : TEXT_WHITE,
+       ALIGNMENTX2, ALIGNMENTY20);
 }
 
 #endif
