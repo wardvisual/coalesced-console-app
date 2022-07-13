cb62b536ff166dda6d830215fd56a7a43b51ca15
Edward <wardvisual@gmail.com>
2022-07-13 18:23:14 +0800

    chore: add hover effect for menu

--- src/core/apps/secondsem/menu/showMenu.cpp
+++ src/core/apps/secondsem/menu/showMenu.cpp
@@ -4,21 +4,27 @@
 
 #include "../../../../includes/constants/alignment.cpp"
 #include "../../../../includes/constants/color.cpp"
+#include "../../../../includes/constants/menu.cpp"
 
 #include "../../../../includes/helpers/color.cpp"
 #include "../../../../includes/helpers/gotoxy.cpp"
 
 #include "../../../components/text.cpp"
 
-void displaySecondSemMenu() {
-
+void displaySecondSemMenu(int selectedMenuItem) {
   text("Second Sem Applications", TEXT_BLUE, ALIGNMENTX38, ALIGNMENTY10);
 
-  text("Activity 01", TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY12);
+  text("Activity 01",
+       selectedMenuItem == MENU_ITEM_ONE ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX38,
+       ALIGNMENTY12);
 
-  text("Activity 02", TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY14);
+  text("Activity 02",
+       selectedMenuItem == MENU_ITEM_TWO ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX38,
+       ALIGNMENTY14);
 
-  text("Activity 03", TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY16);
+  text("Activity 03",
+       selectedMenuItem == MENU_ITEM_THREE ? TEXT_BLUE : TEXT_WHITE,
+       ALIGNMENTX38, ALIGNMENTY16);
 }
 
 #endif
