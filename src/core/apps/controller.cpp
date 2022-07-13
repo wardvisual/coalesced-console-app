4c272364a42fbeff4f5cccb42d0751d7e62d6095
Edward <wardvisual@gmail.com>
2022-07-13 18:21:12 +0800

    chore: simplify menu logic

--- src/core/apps/controller.cpp
+++ src/core/apps/controller.cpp
@@ -1,15 +1,34 @@
 #ifndef CPP_CONTROLLER
 #define CPP_CONTROLLER
 
+#include "../../includes/constants/menu.cpp"
+#include "../../includes/helpers/cleanUp.cpp"
+
+#include "../components/header.cpp"
 #include "../components/input.cpp"
+#include "../components/menu.cpp"
+
+#include "./secondsem/activityOne.cpp"
 #include "./secondsem/menu/showMenu.cpp"
+
+#include "./information/index.cpp"
 
 namespace appController {
 int userInput;
 
-void seconSem() {
-  displaySecondSemMenu();
+void systemInformation() { displaySystemInformation(); }
+
+void secondSem() {
+
+  displaySecondSemMenu(MENU_ITEM_NONE);
+
   input<int>("Input", userInput);
+
+  if (userInput == MENU_ITEM_ONE) {
+    // cleanUpScreen(mainMenuHeaderComponent, headerComponent);
+    displaySecondSemMenu(MENU_ITEM_ONE);
+    activityOne();
+  }
 }
 
 } // namespace appController
