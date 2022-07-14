e12f1bb0a44ad25f2117d4d0cbe559571cd8f188
Edward <wardvisual@gmail.com>
2022-07-14 16:36:14 +0800

    chore: update integer menu item to char

--- src/core/components/menu.cpp
+++ src/core/components/menu.cpp
@@ -18,28 +18,28 @@
  *
  * @param selectedMenuItem The menu item that is currently selected.
  */
-void menuComponent(int selectedMenuItem) {
-  text("01. About the Project",
-       selectedMenuItem == MENU_ITEM_ONE ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+void menuComponent(char selectedMenuItem) {
+  text("[A]. About the Project",
+       selectedMenuItem == MENU_ITEM_A ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
        ALIGNMENTY10);
 
-  text("02. Second Sem Applications",
-       selectedMenuItem == MENU_ITEM_TWO ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+  text("[B]. Second Sem Applications",
+       selectedMenuItem == MENU_ITEM_B ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
        ALIGNMENTY12);
 
-  text("03. Midterm Applications",
-       selectedMenuItem == MENU_ITEM_THREE ? TEXT_BLUE : TEXT_WHITE,
-       ALIGNMENTX2, ALIGNMENTY14);
+  text("[C]. Midterm Applications",
+       selectedMenuItem == MENU_ITEM_C ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+       ALIGNMENTY14);
 
-  text("04. Final Sem Applications",
-       selectedMenuItem == MENU_ITEM_FOUR ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+  text("[D]. Final Sem Applications",
+       selectedMenuItem == MENU_ITEM_D ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
        ALIGNMENTY16);
 
-  text("05. Supplementary Applications",
-       selectedMenuItem == MENU_ITEM_FIVE ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
+  text("[E]. Supplementary Applications",
+       selectedMenuItem == MENU_ITEM_E ? TEXT_BLUE : TEXT_WHITE, ALIGNMENTX2,
        ALIGNMENTY18);
 
-  text("06. Exit", selectedMenuItem == MENU_ITEM_SIX ? TEXT_BLUE : TEXT_WHITE,
+  text("[F]. Exit", selectedMenuItem == MENU_ITEM_F ? TEXT_BLUE : TEXT_WHITE,
        ALIGNMENTX2, ALIGNMENTY20);
 }
 
