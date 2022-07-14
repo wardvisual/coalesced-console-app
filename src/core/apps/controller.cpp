4dafe0ecf6e3adeb712602b15752b5939da8ab7a
Edward <wardvisual@gmail.com>
2022-07-14 16:41:06 +0800

    chore: reference current selected menu and modify it depends on what current menu is selected

--- src/core/apps/controller.cpp
+++ src/core/apps/controller.cpp
@@ -1,7 +1,9 @@
 #ifndef CPP_CONTROLLER
 #define CPP_CONTROLLER
 
+#include "../../includes/constants/alignment.cpp"
 #include "../../includes/constants/menu.cpp"
+#include "../../includes/constants/validation.cpp"
 #include "../../includes/helpers/cleanUp.cpp"
 
 #include "../components/header.cpp"
@@ -18,16 +20,28 @@ int userInput;
 
 void systemInformation() { displaySystemInformation(); }
 
-void secondSem() {
+void secondSem(char &currentSelectedMenu) {
+  int reAlignLabelYCoordinate = ALIGNMENTY31;
+  int reAlignErrorMsgYCoordinate = ALIGNMENTY33;
+
+  std::string inputLabel = "Select Application";
 
   displaySecondSemMenu(MENU_ITEM_NONE);
 
-  input<int>("Input", userInput);
+  input<int>(inputLabel, userInput, IS_RESTRICTED_INPUT,
+             reAlignLabelYCoordinate, reAlignErrorMsgYCoordinate);
 
-  if (userInput == MENU_ITEM_ONE) {
-    // cleanUpScreen(mainMenuHeaderComponent, headerComponent);
-    displaySecondSemMenu(MENU_ITEM_ONE);
+  if (userInput == MENU_ITEM_A) {
+    cleanUpScreen(mainMenuHeaderComponent, headerComponent);
+    displaySecondSemMenu(MENU_ITEM_A);
     activityOne();
+  }
+
+  if (userInput == MENU_ITEM_I) {
+    // cleanUpScreen(mainMenuHeaderComponent, headerComponent);
+    displaySecondSemMenu(MENU_ITEM_A);
+
+    currentSelectedMenu = MENU_ITEM_NONE;
   }
 }
 
