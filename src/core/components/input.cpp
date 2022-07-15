068a7707c2bf42a17c69fd55e6f862e877d3404c
Edward <wardvisual@gmail.com>
2022-07-15 09:10:55 +0800

    chore: clean input component

--- src/core/components/input.cpp
+++ src/core/components/input.cpp
@@ -1,3 +1,5 @@
+/* clang-format off */
+
 /**
  * @file /src/core/components/input.cpp
  * @brief A component that is used to create a complete input style with generic
@@ -5,13 +7,14 @@
  *
  * */
 
+
 #ifndef CPP_INPUT_COMPONENT
 #define CPP_INPUT_COMPONENT
 
 #include "../../includes/constants/alignment.cpp"
 #include "../../includes/helpers/cleanUp.cpp"
+#include "../../includes/helpers/dataType.cpp"
 #include "../../includes/helpers/gotoxy.cpp"
-#include "../../includes/helpers/string.cpp"
 
 #include "../../includes/constants/dataType.cpp"
 
@@ -22,45 +25,26 @@
 #include <iomanip>
 #include <limits>
 
-// void inputStyle(int labelYCoordinate) {
-//   int symbolCount = 31;
-//   std::string generatedSymbol31 = generateSymbol("-", symbolCount);
-
-//   text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2,
-//        labelYCoordinate - ALIGNMENTY1);
-//   text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2,
-//        labelYCoordinate + ALIGNMENTY1);
-// }
+void inputBorder(int labelYCoordinate);
 
 template <typename T>
-void input(std::string &label, T &referenceValue, bool isRestricted,
-           int labelYCoordinate, int errorMsgYCoordinate) {
+void input(std::string &label, T &referenceValue, bool isRestricted, int labelYCoordinate, int errorMsgYCoordinate) {
 
   /* Variables */
   int restrictedInput = 1;
   int maximumInputLength = 20;
-  int symbolCount = 31;
   std::string errorMessage = "Invalid Input. Please try again!";
-  std::string generatedSymbol31 = generateSymbol("-", symbolCount);
 
-  /* Applying input border */
-  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2,
-       labelYCoordinate - ALIGNMENTY1);
-  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2,
-       labelYCoordinate + ALIGNMENTY1);
+  /* Applying border style */
+  inputBorder(labelYCoordinate);
 
   /* Setting up input label properties */
   text(label + ": ", TEXT_WHITE, ALIGNMENTX2, labelYCoordinate);
 
-  /* Lowercase user input if type id is equal to char type id */
-  (!compareTypeId(referenceValue, CHAR_TYPE_ID)) ?: tolower(referenceValue);
-
   /* Validating input */
-  while (!(std::cin >>
-           std::setw(isRestricted ? restrictedInput : maximumInputLength) >>
-           referenceValue) &&
-         !(std::cin.good())) {
-
+  while (std::cin >> std::setw(isRestricted ? restrictedInput : maximumInputLength) 
+          >> referenceValue && !std::cin.good()) {
+            
     /* Displaying error message */
     text(errorMessage, TEXT_LIGHT_RED, ALIGNMENTX2, errorMsgYCoordinate);
 
@@ -72,6 +56,14 @@ void input(std::string &label, T &referenceValue, bool isRestricted,
   /* Cleaning Up */
   std::cin.clear();
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void inputBorder(int labelYCoordinate) {
+  int symbolCount = 31;
+  std::string generatedSymbol31 = generateSymbol("-", symbolCount);
+
+  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2, labelYCoordinate - ALIGNMENTY1);
+  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2, labelYCoordinate + ALIGNMENTY1);
 }
 
 #endif
