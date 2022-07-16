8fae5eefbdee403a88b9c706f2f99be71276dca1
Edward <wardvisual@gmail.com>
2022-07-16 13:12:47 +0800

    chore: fix bugs

--- src/core/components/input.cpp
+++ src/core/components/input.cpp
@@ -1,4 +1,4 @@
-/* clang-format off */
+/* clang-format on */
 
 /**
  * @file /src/core/components/input.cpp
@@ -7,15 +7,14 @@
  *
  * */
 
-
 #ifndef CPP_INPUT_COMPONENT
 #define CPP_INPUT_COMPONENT
 
 #include "../../includes/constants/alignment.cpp"
 #include "../../includes/helpers/cleanUp.cpp"
 #include "../../includes/helpers/dataType.cpp"
-#include "../../includes/helpers/search.cpp"
 #include "../../includes/helpers/gotoxy.cpp"
+#include "../../includes/helpers/search.cpp"
 
 #include "../../includes/constants/dataType.cpp"
 
@@ -29,13 +28,15 @@
 void inputBorder(int labelYCoordinate);
 
 template <typename T>
-void input(std::string &label, T &referenceValue, T arrayValues[], bool isRestricted, int labelYCoordinate, int errorMsgYCoordinate) {
+void input(std::string &label, T &referenceValue, T arrayValues[],
+           int arrayLength, bool isRestricted, int labelYCoordinate,
+           int errorMsgYCoordinate) {
 
   /* Variables */
   int restrictedInput = 1;
   int maximumInputLength = 20;
   int nonValidArrayLength = 0;
-  int firstIndex = 0; 
+  int firstIndex = 0;
   std::string errorMessage = "Invalid Input. Please try again!";
 
   /* Applying border style */
@@ -43,31 +44,37 @@ void input(std::string &label, T &referenceValue, T arrayValues[], bool isRestri
 
   /* Setting up input label properties */
   text(label + ": ", TEXT_WHITE, ALIGNMENTX2, labelYCoordinate);
-  
+
   /* If user input is char type then lowercase */
-  if(compareTypeId(referenceValue, CHAR_TYPE_ID)) {
+  if (compareTypeId(referenceValue, CHAR_TYPE_ID)) {
     lowerCaseCharacter(referenceValue);
   }
-  
+
   /* Validating input for char and string type */
-  if(sizeof(arrayValues) / sizeof(arrayValues[firstIndex]) > nonValidArrayLength) {
-    std::cin >> std::setw(isRestricted ? restrictedInput : maximumInputLength) >> referenceValue;
-    
-    while (!isInArray<T>(arrayValues, sizeof(arrayValues) / sizeof(arrayValues[firstIndex]), referenceValue)) {
-      std::cin >> std::setw(isRestricted ? restrictedInput : maximumInputLength) >> referenceValue;
-      
-       /* Displaying error message */
+  if (arrayLength > nonValidArrayLength) {
+    std::cin >>
+        std::setw(isRestricted ? restrictedInput : maximumInputLength) >>
+        referenceValue;
+
+    while (!isInArray<T>(arrayValues, arrayLength, referenceValue)) {
+      /* Displaying error message */
       text(errorMessage, TEXT_LIGHT_RED, ALIGNMENTX2, errorMsgYCoordinate);
-      
+
       /* Clean Up */
       std::cin.clear();
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+      std::cin >>
+          std::setw(isRestricted ? restrictedInput : maximumInputLength) >>
+          referenceValue;
     }
   } else {
     /* for integer type */
-    while (std::cin >> std::setw(isRestricted ? restrictedInput : maximumInputLength) 
-            >> referenceValue && !std::cin.good()) {
-              
+    while (std::cin >>
+               std::setw(isRestricted ? restrictedInput : maximumInputLength) >>
+               referenceValue &&
+           !std::cin.good()) {
+
       /* Displaying error message */
       text(errorMessage, TEXT_LIGHT_RED, ALIGNMENTX2, errorMsgYCoordinate);
 
@@ -86,8 +93,10 @@ void inputBorder(int labelYCoordinate) {
   int symbolCount = 31;
   std::string generatedSymbol31 = generateSymbol("-", symbolCount);
 
-  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2, labelYCoordinate - ALIGNMENTY1);
-  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2, labelYCoordinate + ALIGNMENTY1);
+  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2,
+       labelYCoordinate - ALIGNMENTY1);
+  text(generatedSymbol31, TEXT_WHITE, ALIGNMENTX2,
+       labelYCoordinate + ALIGNMENTY1);
 }
 
 #endif
