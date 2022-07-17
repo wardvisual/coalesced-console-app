9fa24649d8071404de6b9f558bf5a4aa10136cbc
Edward <wardvisual@gmail.com>
2022-07-17 11:26:29 +0800

    chore: update menu constant

--- src/includes/constants/menu.cpp
+++ src/includes/constants/menu.cpp
@@ -7,27 +7,27 @@
 #ifndef CPP_MENU_CONSTANT
 #define CPP_MENU_CONSTANT
 
-const char MENU_ITEM_NONE = '-';
-const char MENU_ITEM_A = 'a';
-const char MENU_ITEM_B = 'b';
-const char MENU_ITEM_C = 'c';
-const char MENU_ITEM_D = 'd';
-const char MENU_ITEM_E = 'e';
-const char MENU_ITEM_F = 'f';
-const char MENU_ITEM_G = 'g';
-const char MENU_ITEM_H = 'h';
-const char MENU_ITEM_I = 'i';
-const char MENU_ITEM_J = 'j';
+const std::string MENU_ITEM_NONE = "-";
+const std::string MENU_ITEM_A = "a";
+const std::string MENU_ITEM_B = "b";
+const std::string MENU_ITEM_C = "c";
+const std::string MENU_ITEM_D = "d";
+const std::string MENU_ITEM_E = "e";
+const std::string MENU_ITEM_F = "f";
+const std::string MENU_ITEM_G = "g";
+const std::string MENU_ITEM_H = "h";
+const std::string MENU_ITEM_I = "i";
+const std::string MENU_ITEM_J = "j";
 
-const char MENU_ITEM_A2 = 'I';
-const char MENU_ITEM_B2 = 'B';
-const char MENU_ITEM_C2 = 'C';
-const char MENU_ITEM_D2 = 'D';
-const char MENU_ITEM_E2 = 'E';
-const char MENU_ITEM_F2 = 'F';
-const char MENU_ITEM_G2 = 'G';
-const char MENU_ITEM_H2 = 'H';
-const char MENU_ITEM_I2 = 'I';
-const char MENU_ITEM_J2 = 'J';
+const std::string MENU_ITEM_A2 = "I";
+const std::string MENU_ITEM_B2 = "B";
+const std::string MENU_ITEM_C2 = "C";
+const std::string MENU_ITEM_D2 = "D";
+const std::string MENU_ITEM_E2 = "E";
+const std::string MENU_ITEM_F2 = "F";
+const std::string MENU_ITEM_G2 = "G";
+const std::string MENU_ITEM_H2 = "H";
+const std::string MENU_ITEM_I2 = "I";
+const std::string MENU_ITEM_J2 = "J";
 
 #endif
