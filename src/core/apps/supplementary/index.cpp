bea64801bb1888dfb244b91b41651e66fdf2c1f5
Edward <wardvisual@gmail.com>
2022-07-24 07:41:43 +0800

    chore: fix bugs

--- src/core/apps/supplementary/index.cpp
+++ src/core/apps/supplementary/index.cpp
@@ -1,76 +1,108 @@
 /**
- * @file /src/apps/secondsem/index.cpp
- * @brief
+ * @file /src/apps/supplementary/index.cpp
+ * @brief Supplementary applications
  *
  * */
 
-#ifndef CPP_INDEX_SUPPLEMENTARY
-#define CPP_INDEX_SUPPLEMENTARY
+/* A preprocessor directive that prevents the file from being included more than
+  once. */
+#ifndef CPP_INDEX_SUPPLEMENTARY_
+#define CPP_INDEX_SUPPLEMENTARY_
 
-#define MAX_MENU_ITEM_LENGTH 9
+/* A constant that is used to determine the length of the array. */
+#define MAX_SUPPLEMENTARY_MENU_ITEM_LENGTH 4
 
-#include "activityOne/index.cpp"
+/* Supplementary activitities*/
+#include "studentManagement/index.cpp"
 
+/* Supplementary menu */
+#include "./menu/showMenu.cpp"
+
+/* Controllers */
 #include "../../controllers/menu.cpp"
 
+/* Constants */
 #include "../../../includes/constants/alignment.cpp"
+#include "../../../includes/constants/appType.cpp"
 #include "../../../includes/constants/menu.cpp"
 #include "../../../includes/constants/validation.cpp"
 
+/* Helpers */
 #include "../../../includes/helpers/cleanUp.cpp"
 
+/* Components */
 #include "../../components/header.cpp"
 #include "../../components/input.cpp"
 #include "../../components/menu.cpp"
 
-void (*secondSemFunctions[MAX_MENU_ITEM_LENGTH])() = {activityOne};
+void (*supplementaryMethods[MAX_SUPPLEMENTARY_MENU_ITEM_LENGTH])(
+    void (&previousMenu)(std::string menuType)) = {studentManagement};
 
-void supplementary(std::string &currentSelectedMenu,
-                   void (&previousFunctionCaller)()) {
-  int reAlignLabelYCoordinate = ALIGNMENTY31;
-  int reAlignErrorMsgYCoordinate = ALIGNMENTY33;
+/**
+ * It handles second sem menu
+ *
+ * @param currentSelectedMenu This is the current selected menu item.
+ * @param previousFunctionCaller This is the function that will be called when
+ * the user exits the menu.
+ */
+void renderSupplementaryApp(std::string &currentSelectedMenu,
+                            void (&previousFunctionCaller)()) {
 
-  std::string inputLabel = "Select Application";
-  int firstIndex = 0;
-  int maxMenuLength = 9;
+  /* Declaring variables. */
+  int maxMenuLength = 4;
+  std::string userInput;
+  std::string backMenu = MENU_ITEM_4;
+  int reAlignLabelYCoordinate = ALIGNMENTY21;
+  int reAlignErrorMsgYCoordinate = ALIGNMENTY25;
+  std::string inputLabel = "Select Supplementary App";
   std::string expectedArrayOfValue[maxMenuLength] = {MENU_ITEM_1, MENU_ITEM_2,
-                                                     MENU_ITEM_3};
+                                                     MENU_ITEM_3, MENU_ITEM_4};
 
-  displaySecondSemMenu(MENU_ITEM_NONE);
+  // /* A function that clears the screen. */
+  cleanUpScreen(mainMenuHeaderComponent, headerComponent);
 
-  input<std::string>(inputLabel, userInput, expectedArrayOfValue,
+  /* Calling the function `renderSupplementaryAppMenu` and passing the value
+    `MENU_ITEM_NONE` to it. */
+  renderSupplementaryAppMenu(MENU_ITEM_NONE);
+
+  /* It handles user input and prints an error if there is an error. */
+  input<std::string>(inputLabel, userInput, expectedArrayOfValue, maxMenuLength,
                      RESTRICTED_INPUT, reAlignLabelYCoordinate,
                      reAlignErrorMsgYCoordinate);
 
-  std::string foundedElement =
-      findElement(expectedArrayOfValue, maxMenuLength, userInput);
+  /* A loop that handles user input and prints an error if there is an error.*/
+  while (userInput != backMenu) {
+    /* Finding the element in the array. */
+    std::string foundElement = findElement<std::string>(
+        expectedArrayOfValue, maxMenuLength, userInput);
 
-  if (userInput == foundedElement) {
-    std::string currentMenuItem = userInput;
-    int exitMenu = MENU_ITEM_9;
-
-    while (foundedElement != exitMenu) {
-      cleanUpScreen(mainMenuHeaderComponent, headerComponent);
-      displaySecondSemMenu(foundedElement);
-
-      for (int i = 0; i < sizeof(expectedArrayOfValue) /
-                              sizeof(expectedArrayOfValue[firstIndex]);
-           i++) {
-        if (selecteMenu == expectedArrayOfValue[i]) {
-          (*secondSemFunctions[i])();
+    /* Checking if the user input is equal to the found element. If it is, it
+    will call the function that is in the array. */
+    if (userInput == foundElement) {
+      for (int i = 0; i < maxMenuLength; i++) {
+        if (foundElement == expectedArrayOfValue[i]) {
+          cleanUpScreen(mainMenuHeaderComponent, headerComponent);
+          renderSupplementaryAppMenu(foundElement);
+          (*supplementaryMethods[i])(renderSupplementaryAppMenu);
         }
       }
+
+      /* It handles user input and prints an error if there is an error. */
+      input<std::string>(inputLabel, userInput, expectedArrayOfValue,
+                         maxMenuLength, RESTRICTED_INPUT,
+                         reAlignLabelYCoordinate, reAlignErrorMsgYCoordinate);
     }
-
-    displaySecondSemMenu(MENU_ITEM_NONE);
-    cleanUpScreen(mainMenuHeaderComponent, headerComponent);
-
-    /* Rerendering previous function caller*/
-    previousFunctionCaller();
-
-    // exit
-    currentSelectedMenu = MENU_ITEM_NONE;
   }
+
+  /* Calling the function `renderSupplementaryAppMenu` and passing the value
+     `MENU_ITEM_NONE` to it. */
+  renderSupplementaryAppMenu(MENU_ITEM_NONE);
+  /* A function that clears the screen. */
+  cleanUpScreen(mainMenuHeaderComponent, headerComponent);
+  /* renders previous function caller*/
+  previousFunctionCaller();
+  /* exit */
+  currentSelectedMenu = MENU_ITEM_NONE;
 }
 
 #endif
