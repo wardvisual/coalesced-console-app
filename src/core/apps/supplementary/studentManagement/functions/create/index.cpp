4004a8c5c200da89df6262e8b7d31cebf7fc3292
Edward <wardvisual@gmail.com>
2022-07-24 13:46:47 +0800

    chore: finish the functionality of create student

--- src/core/apps/supplementary/studentManagement/functions/create/index.cpp
+++ src/core/apps/supplementary/studentManagement/functions/create/index.cpp
@@ -23,9 +23,35 @@
 
 /* Helpers */
 #include "../../../../../../includes/helpers/cleanUp.cpp"
+#include "../../../../../../includes/mockData/students.cpp"
 
-void create(std::string type, std::string currentMenu) {
-  std::cout << "createeeeeeeeeee";
+void create(std::string type, std::string currentMenu, void (&heading)()) {
+  int reAlignLabelYCoordinate = ALIGNMENTY25,
+      reAlignErrorMsgYCoordinate = ALIGNMENTY29;
+  std::string inputLabelForAddStudentName = "Enter Student Name";
+  std::string name;
+
+  int maxMenuLength = 0;
+  std::string expectedArrayOfValue[] = {};
+
+  /* A function that clears the newly updated text on a screen and displays
+      the previous screen state. */
+  reViewMainScreen(type, currentMenu, heading);
+
+  input<std::string>(inputLabelForAddStudentName, name, expectedArrayOfValue,
+                     maxMenuLength, !RESTRICTED_INPUT, reAlignLabelYCoordinate,
+                     reAlignErrorMsgYCoordinate);
+
+  /* A function that clears the newly updated text on a screen and displays
+    the previous screen state. */
+  reViewMainScreen(type, currentMenu, heading);
+
+  mockData::students.push_back(name);
+
+  text("Result:", TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY15);
+  text("Message: Student successfully added!", TEXT_WHITE, ALIGNMENTX38,
+       ALIGNMENTY17);
+  text("Student Name: " + name, TEXT_WHITE, ALIGNMENTX38, ALIGNMENTY19);
 }
 
 #endif
