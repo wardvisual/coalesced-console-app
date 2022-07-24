8fbf2c2f5aacd16a0ee4f527d45de9dd61335291
Edward <wardvisual@gmail.com>
2022-07-24 13:46:18 +0800

    chore: update array length and exit menu

--- src/core/apps/supplementary/studentManagement/index.cpp
+++ src/core/apps/supplementary/studentManagement/index.cpp
@@ -9,6 +9,8 @@
 #ifndef CPP_STUDENT_MANAGEMENT_SUPPLEMENTARY
 #define CPP_STUDENT_MANAGEMENT_SUPPLEMENTARY
 
+#define MAX_STUDENT_MENU_METHODS_LENGTH 5
+
 /* Student Management operations */
 #include "./functions/create/index.cpp"
 #include "./functions/delete/index.cpp"
@@ -33,20 +35,20 @@
 /* Helpers */
 #include "../../../../includes/helpers/cleanUp.cpp"
 
-void (*studentManagementMethods[4])(std::string type, std::string currentMenu,
-                                    void (&heading)()) = {view, create, edit,
-                                                          deleteStudent};
+void (*studentManagementMethods[MAX_STUDENT_MENU_METHODS_LENGTH])(
+    std::string type, std::string currentMenu,
+    void (&heading)()) = {view, create, edit, deleteStudent};
 
 void studentManagement(void (&previousMenu)(std::string menuType)) {
   /* Declaring variables. */
-  int maxMenuLength = 4;
+  int maxMenuLength = 5;
   std::string userInput;
-  std::string backMenu = MENU_ITEM_4;
+  std::string backMenu = MENU_ITEM_5;
   int reAlignLabelYCoordinate = ALIGNMENTY25;
   int reAlignErrorMsgYCoordinate = ALIGNMENTY29;
   std::string inputLabel = "Select Operation";
-  std::string expectedArrayOfValue[maxMenuLength] = {MENU_ITEM_1, MENU_ITEM_2,
-                                                     MENU_ITEM_3, MENU_ITEM_4};
+  std::string expectedArrayOfValue[maxMenuLength] = {
+      MENU_ITEM_1, MENU_ITEM_2, MENU_ITEM_3, MENU_ITEM_4, MENU_ITEM_5};
 
   /* A function that clears the screen. */
   cleanUpScreen(mainMenuHeaderComponent, headerComponent);
